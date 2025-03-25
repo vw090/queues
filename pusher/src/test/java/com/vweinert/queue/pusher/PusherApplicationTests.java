b262416b6b63fc0a941787bd9a911a41ef794591
Victor Weinert <dev@vweinert.com>
2025-03-25 18:11:54 -0500

    first commit

---
+++ pusher/src/test/java/com/vweinert/queue/pusher/PusherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vweinert.queue.pusher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PusherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ receiver/src/test/java/com/vweinert/queue/receiver/ReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vweinert.queue.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
